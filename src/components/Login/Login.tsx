1ef62ca7226a8f5467a38328fe3f4613c086cf94
Anton lystsov <anton.lyscov@yandex.ru>
2023-06-02 13:12:49 +0300

    исправлена проблема регистрации и логина

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -12,7 +12,7 @@ type LoginData = {
     password: string;
 };
 
-function Login(props: PropsLogin): JSX.Element | null{
+function Login(props: PropsLogin): JSX.Element {
 
     const { setIsLoggedIn, setUserData, isLoggedIn } = props;
 
@@ -49,54 +49,53 @@ function Login(props: PropsLogin): JSX.Element | null{
 
     if (isLoggedIn) {
         navigate('/movies');
+        return <></>
     } else {
-        return null;
-    }
-
-    return (
-        <section className="login">
-            <div className="login__header">
-                <Link to='/'>
-                    <img src={logo} alt="logo" />
-                </Link>
-                <h1 className="login__title">Рады видеть!</h1>
-            </div>
-            <form className="login__form" onSubmit={handleSubmit}>
-                <label htmlFor="" className="login__form-label">E-mail</label>
-                <input
-                    type="email"
-                    className="login__form-input"
-                    pattern={EMAIL_PATTERN}
-                    name="email"
-                    minLength={4}
-                    maxLength={40}
-                    value={values.email}
-                    onChange={handleChange}
-                    required
-                />
-                <span className="login__form-span">{errors['email']}</span>
-                <label htmlFor="" className="login__form-label">Пароль</label>
-                <input
-                    type="password"
-                    name="password"
-                    className="login__form-input"
-                    minLength={8}
-                    maxLength={50}
-                    value={values.password}
-                    onChange={handleChange}
-                    required
-                />
-                <span className="login__form-span">{errors['password']}</span>
-                <button className={!isValid ? 'login__button login__button-inactive' : 'login__button'} disabled={!isValid}>Войти</button>
-            </form>
-            <div className="login__footer">
-                <div className='login__footer-flex'>
-                    <h2 className="login__footer-title">Ещё не зарегистрированы?</h2>
-                    <a className="login__footer-signin" href="/signup">Регистрация</a>
+        return (
+            <section className="login">
+                <div className="login__header">
+                    <Link to='/'>
+                        <img src={logo} alt="logo" />
+                    </Link>
+                    <h1 className="login__title">Рады видеть!</h1>
                 </div>
-            </div>
-        </section>
-    )
+                <form className="login__form" onSubmit={handleSubmit}>
+                    <label htmlFor="" className="login__form-label">E-mail</label>
+                    <input
+                        type="email"
+                        className="login__form-input"
+                        pattern={EMAIL_PATTERN}
+                        name="email"
+                        minLength={4}
+                        maxLength={40}
+                        value={values.email}
+                        onChange={handleChange}
+                        required
+                    />
+                    <span className="login__form-span">{errors['email']}</span>
+                    <label htmlFor="" className="login__form-label">Пароль</label>
+                    <input
+                        type="password"
+                        name="password"
+                        className="login__form-input"
+                        minLength={8}
+                        maxLength={50}
+                        value={values.password}
+                        onChange={handleChange}
+                        required
+                    />
+                    <span className="login__form-span">{errors['password']}</span>
+                    <button className={!isValid ? 'login__button login__button-inactive' : 'login__button'} disabled={!isValid}>Войти</button>
+                </form>
+                <div className="login__footer">
+                    <div className='login__footer-flex'>
+                        <h2 className="login__footer-title">Ещё не зарегистрированы?</h2>
+                        <a className="login__footer-signin" href="/signup">Регистрация</a>
+                    </div>
+                </div>
+            </section>
+        )
+    }
 }
 
 export default Login;
--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -56,66 +56,68 @@ function Register(props: PropsLogin) {
 
     if (isLoggedIn) {
         navigate('/movies');
+        return <></>
     } else {
-        return null;
-    }
 
-    return (
-        <section className="register">
-            <div className="register__header">
-                <Link to='/'>
-                    <img src={logo} alt="logo" />
-                </Link>
-                <h1 className="register__title">Добро пожаловать!</h1>
-            </div>
-            <form className="register__form" onSubmit={handleSubmit}>
-                <label htmlFor="" className="register__form-label">Имя</label>
-                <input
-                    type="text"
-                    className="register__form-input"
-                    name="name"
-                    minLength={4}
-                    maxLength={40}
-                    value={values.name}
-                    onChange={handleChange}
-                    required
-                />
-                <span className="register__form-span">{errors['name']}</span>
-                <label htmlFor="" className="register__form-label">E-mail</label>
-                <input
-                    type="email"
-                    className="register__form-input"
-                    name="email"
-                    minLength={4}
-                    maxLength={40}
-                    pattern={EMAIL_PATTERN}
-                    value={values.email}
-                    onChange={handleChange}
-                    required
-                />
-                <span className="register__form-span">{errors['email']}</span>
-                <label htmlFor="" className="register__form-label">Пароль</label>
-                <input
-                    type="password"
-                    className="register__form-input"
-                    name="password"
-                    minLength={8}
-                    maxLength={50}
-                    onChange={handleChange}
-                    value={values.password}
-                    required
-                />
-                <span className="register__form-span">{errors['password']}</span>
-                <button className={!isValid ? 'register__button register__button-inactive' : 'register__button'} disabled={!isValid}>Зарегистрироваться</button>
-            </form>
-            <div className="register__footer">
-                <div className='register__footer-flex'>
-                    <h2 className="register__footer-title">Уже зарегистрированы?</h2>
-                    <a className="register__footer-signin" href="/signin">Войти</a>
+
+
+        return (
+            <section className="register">
+                <div className="register__header">
+                    <Link to='/'>
+                        <img src={logo} alt="logo" />
+                    </Link>
+                    <h1 className="register__title">Добро пожаловать!</h1>
                 </div>
-            </div>
-        </section>
-    )
+                <form className="register__form" onSubmit={handleSubmit}>
+                    <label htmlFor="" className="register__form-label">Имя</label>
+                    <input
+                        type="text"
+                        className="register__form-input"
+                        name="name"
+                        minLength={4}
+                        maxLength={40}
+                        value={values.name}
+                        onChange={handleChange}
+                        required
+                    />
+                    <span className="register__form-span">{errors['name']}</span>
+                    <label htmlFor="" className="register__form-label">E-mail</label>
+                    <input
+                        type="email"
+                        className="register__form-input"
+                        name="email"
+                        minLength={4}
+                        maxLength={40}
+                        pattern={EMAIL_PATTERN}
+                        value={values.email}
+                        onChange={handleChange}
+                        required
+                    />
+                    <span className="register__form-span">{errors['email']}</span>
+                    <label htmlFor="" className="register__form-label">Пароль</label>
+                    <input
+                        type="password"
+                        className="register__form-input"
+                        name="password"
+                        minLength={8}
+                        maxLength={50}
+                        onChange={handleChange}
+                        value={values.password}
+                        required
+                    />
+                    <span className="register__form-span">{errors['password']}</span>
+                    <button className={!isValid ? 'register__button register__button-inactive' : 'register__button'} disabled={!isValid}>Зарегистрироваться</button>
+                </form>
+                <div className="register__footer">
+                    <div className='register__footer-flex'>
+                        <h2 className="register__footer-title">Уже зарегистрированы?</h2>
+                        <a className="register__footer-signin" href="/signin">Войти</a>
+                    </div>
+                </div>
+            </section>
+        )
+    }
 }
 
 export default Register;
